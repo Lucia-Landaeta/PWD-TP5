2d5a93f3b9ef86aac840eeef353f0a29b53c81e3
Lucia-Landaeta <lucia.landaeta@est.fi.uncoma.edu.ar>
2021-10-10 16:24:49 -0300

    -

--- autenticacion.sql
+++ autenticacion.sql
@@ -70,6 +70,7 @@ INSERT INTO `usuario`(`usNombre`, `usPass`, `usMail`) VALUES ('Laura.H','1cbf75c
 INSERT INTO `usuario`(`usNombre`, `usPass`, `usMail`) VALUES ('Luis.V','72ede98b2623f48a8c4d24600dea2edb','L.S@mail.com');
 INSERT INTO `usuario`(`usNombre`, `usPass`, `usMail`) VALUES ('Jose.V','f0ce7fdefac92e3eb28fac3adedc87b7','J.V@mail.com');
 INSERT INTO `usuario`(`usNombre`, `usPass`, `usMail`) VALUES ('Romina.E','17ca37f512b6e267e85d2b9638fa715c','R.E@mail.com');
+INSERT INTO `usuario`(`usNombre`, `usPass`, `usMail`,`usDeshabilitado`) VALUES ('Joaquin.P','aa6c3a844e0aa7c13c03cff70e586bf2','J.P@mail.com','2021-10-10 16:05:30');
 
 INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('1','1');
 INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('2','3');
@@ -77,7 +78,7 @@ INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('3','2');
 INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('2','2');
 INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('4','2');
 INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('5','3');
-INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('6','3');
+INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('7','3');
 
 -- Us: Sonia.J pass: sj1234
 -- Us: Luis.V pass: lv2222
@@ -85,5 +86,6 @@ INSERT INTO `usuariorol`(`idUsuario`, `idRol`) VALUES ('6','3');
 -- Us: Laura.H pass: lh4444
 -- Us: Jose.V pass: jv3333
 -- Us: Romina.E pass: re5555
+-- Us: Joaquin.P pass: jp6666
 
 
