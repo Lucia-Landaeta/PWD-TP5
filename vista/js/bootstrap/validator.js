d888f4b53deae0efea6b332e6c715b4af136c6e9
Lucia-Landaeta <lucia.landaeta@est.fi.uncoma.edu.ar>
2021-10-11 11:35:14 -0300

    -

--- vista/js/bootstrap/validator.js
+++ vista/js/bootstrap/validator.js
@@ -38,3 +38,43 @@ $('#login').bootstrapValidator({
         .filter('[data-bv-validator="' + data.validator + '"]').show();
 });
 
+
+$('#actualizarLog').bootstrapValidator({
+    message: 'Este valor no es valido',
+    feedbackIcons: {
+        valid: 'fa fa-check',
+        invalid: 'fa fa-exclamation',
+        validating: 'fa fa-circle'
+    },
+    fields: {
+        usNombre: {
+            validators: {
+                notEmpty: {
+                    message : 'Debe ingresar un nombre'
+                },
+                regexp: {
+                    regexp: /^[a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑüÜ_.\s]+$/,
+                    message: 'Solo se permiten letras _ y .'
+                }
+            }
+        },
+        usMail: {
+            validators: {
+                notEmpty: {
+                    message : 'Debe ingresar un correo'
+                },
+                regexp: {
+                    regexp: /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/,
+                    message: 'No es un correo valido'
+                }
+            }
+        },
+        
+    }
+})
+.on('error.validator.bv', function(e, data) {
+    data.element
+        .data('bv.messages')
+        .find('.help-block[data-bv-for="' + data.field + '"]').hide()
+        .filter('[data-bv-validator="' + data.validator + '"]').show();
+});
