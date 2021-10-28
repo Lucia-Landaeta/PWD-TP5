ec448a801bc64525baab68d91723c5b941a5c882
Raam4 <ramiro_cardozo22@hotmail.com>
2021-10-28 18:00:53 -0300

    office

--- modelo/DB.php
+++ modelo/DB.php
@@ -1,6 +1,4 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT']."/PWD-TP5/utiles/idiorm.php";
-include_once $_SERVER['DOCUMENT_ROOT']."/PWD-TP5/utiles/paris.php";
 class DB extends Model{
     public function __construct(){
         ORM::configure(array(
